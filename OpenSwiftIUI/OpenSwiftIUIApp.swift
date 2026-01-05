1a15405451e08ba97dd69021f08963aa01d17077
Amos Gyamfi <amosgyamfi.fi@gmail.com>
2026-01-05 13:17:00 +0200

    Initial Commit

---
+++ OpenSwiftIUI/OpenSwiftIUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenSwiftIUIApp.swift
+//  OpenSwiftIUI
+//
+//  Created by Amos Gyamfi on 5.1.2026.
+//
+
+import SwiftUI
+
+@main
+struct OpenSwiftIUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
